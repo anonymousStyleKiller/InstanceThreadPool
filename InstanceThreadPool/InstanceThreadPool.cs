4b06ac3c2434c945016e03f9c5c8f16ddfe591cc
Anton Kharchenko <gumb1t97@gmail.com>
2022-04-04 16:30:36 +0300

    :art: feature: Preparing for pull of threads
    Created an array of threads and thread started

---
+++ InstanceThreadPool/InstanceThreadPool.cs
@@ -0,0 +1,48 @@
+﻿namespace InstanceThreadPool;
+
+public class InstanceThreadPool
+{
+    private readonly ThreadPriority _priority;
+    private readonly string? _name;
+    private readonly Thread[] _threads;
+
+    public InstanceThreadPool(int maxThreadsCount, ThreadPriority priority = ThreadPriority.Normal, string? name =null)
+    {
+        if (maxThreadsCount <= 0) 
+            throw new ArgumentOutOfRangeException(nameof(maxThreadsCount), 
+                                                  maxThreadsCount, "Count of pool must be more than 1 or equals.");
+
+        _priority = priority;
+        _name = name;
+        _threads = new Thread[maxThreadsCount];
+    }
+
+    private void Initialize()
+    {
+        for (var i = 0; i < _threads.Length; i++)
+        {
+            var name = $"{nameof(InstanceThreadPool)[(GetHashCode())]}-Thread{i}";
+            
+            var thread = new Thread(WorkingThread)
+            {
+                Name = name,
+                IsBackground = true,
+                Priority = _priority,
+            };
+            _threads[i] = thread;
+            thread.Start();
+        }
+    }
+
+    public void Execute(Action work) => Execute(null, _=>work());
+
+    private void Execute(object? Parameter, Action<object?> work)
+    {
+        
+    }
+    
+    private void WorkingThread()
+    {
+        
+    }
+}
